1072d7fb3e0427ed6e48515d351de79ef6ea850e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 23:51:01 +0800

    dsds

--- post-types/course/shortcodes/course-list/templates/layout-collections/standard.php
+++ post-types/course/shortcodes/course-list/templates/layout-collections/standard.php
@@ -16,22 +16,13 @@
 
 			</div>
 			<?php echo academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/excerpt', '', $params); ?>
-			<div class="eltdf-cli-bottom-info">
-				<?php if ($enable_students == 'yes') {
-					echo academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/students', '', $params);
-				} ?>
-
-				<?php if ($enable_ratings == 'yes') {
-					echo academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/ratings', '', $params);
-				} ?>
-
-				<?php if ($enable_category == 'yes' && $category_boxed == 'no') {
-					echo academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/category', '', $params);
-				} ?>
-
-				<?php if ($enable_price == 'yes') {
-					echo academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/price', '', $params);
-				} ?>
+			<div class="eltdf-cli-bottom-info d-flex">
+				<div class="price">
+					<?= academist_lms_get_cpt_shortcode_module_template_part('course', 'course-list', 'parts/price', '', $params); ?>
+					<a itemprop="url" href="<?php the_permalink() ?>" target="_self" style="color: #ffffff;margin: 0 15px 0 0" class="eltdf-btn eltdf-btn-medium eltdf-btn-solid eltdf-btn-arrow button-accent">
+						<span class="eltdf-btn-text">Read More</span>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
