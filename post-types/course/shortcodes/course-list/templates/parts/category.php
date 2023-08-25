cddff48645eccff7e4c91fb953777f375c76be54
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 23:32:32 +0800

    ccc

--- post-types/course/shortcodes/course-list/templates/parts/category.php
+++ post-types/course/shortcodes/course-list/templates/parts/category.php
@@ -1,18 +1,1 @@
-<?php
-$level = get__post_meta('level');
-?>
-
-<?php if ($level) { ?>
-
-	<div class="eltdf-cli-category-holder">
-		<div class="level-holder">
-			<span class="icon">
-				<svg xmlns="http://www.w3.org/2000/svg" width="15.008" height="20.034" viewBox="0 0 15.008 20.034">
-					<path id="award" d="M14.2,15.344a5.5,5.5,0,0,0,1.977-.884l1.147,4.507a1.675,1.675,0,0,1-2.392,1.859L13,19.9a2.32,2.32,0,0,0-2,0l-1.933.928a1.675,1.675,0,0,1-2.392-1.859L7.82,14.46a5.5,5.5,0,0,0,1.977.884m4.4,0a9.95,9.95,0,0,1-4.4,0m4.4,0a5.342,5.342,0,0,0,4.043-3.915,9.052,9.052,0,0,0,0-4.266A5.342,5.342,0,0,0,14.2,3.247a9.95,9.95,0,0,0-4.4,0A5.342,5.342,0,0,0,5.755,7.162a9.055,9.055,0,0,0,0,4.266A5.342,5.342,0,0,0,9.8,15.344" transform="translate(-4.496 -1.996)" fill="none" stroke="#fff" stroke-width="2" />
-				</svg>
-			</span>
-			<span><?= $level ?></span>
-		</div>
-	</div>
-
-<?php } ?>
+<?php do_action('course_level') ?>
