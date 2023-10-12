36e274a1b441ca177e685331e43d6dbedcc01bfd
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 23:55:24 +0800

    Update info-bellow.php

--- post-types/instructor/shortcodes/instructor/templates/info-bellow.php
+++ post-types/instructor/shortcodes/instructor/templates/info-bellow.php
@@ -1,5 +1,5 @@
 
-<div class="eltdf-instructor eltdf-item-space <?php echo esc_attr( $instructor_layout ) ?> <?php echo esc_attr( $instructor_skin ) ?>">
+<div class="eltdf-instructor eltdf-item-space <?php echo esc_attr( $instructor_layout ) ?> <?php echo esc_attr( $instructor_skin ) ?>">ssssxxs
 	<div class="eltdf-instructor-inner">
 		<?php if ( get_the_post_thumbnail( $instructor_id ) !== '' ) { ?>
 			<div class="eltdf-instructor-image">
