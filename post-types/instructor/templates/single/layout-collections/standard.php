19f3c751efe73e2d43da1ab0695e2dfd4fe73b27
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-13 17:53:20 +0800

    Update standard.php

--- post-types/instructor/templates/single/layout-collections/standard.php
+++ post-types/instructor/templates/single/layout-collections/standard.php
@@ -1,17 +1,19 @@
 <div class="eltdf-instructor-single-info-holder">
 	<div class="eltdf-grid-row">
 		<div class="eltdf-ts-info-holder eltdf-grid-col-3">
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/image', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/name', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/title', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/vita', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/social', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/email', 'instructor', '', $params ); ?>
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/resume', 'instructor', '', $params ); ?>
-			<?php dynamic_sidebar('instructor_sidebar') ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/image', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/name', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/title', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/vita', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/social', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/email', 'instructor', '', $params); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/resume', 'instructor', '', $params); ?>
+			<aside class="eltdf-sidebar">
+				<?php dynamic_sidebar('instructor_sidebar') ?>
+			</aside>
 		</div>
 		<div class="eltdf-ts-content-holder eltdf-grid-col-9">
-			<?php academist_lms_get_cpt_single_module_template_part( 'single/parts/tabs', 'instructor', '', $params ); ?>
+			<?php academist_lms_get_cpt_single_module_template_part('single/parts/tabs', 'instructor', '', $params); ?>
 		</div>
 	</div>
 </div>
