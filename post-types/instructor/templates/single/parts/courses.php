1c517323ecd0e51b1d00ff0140ae34ccb13c7657
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 02:52:34 +0800

    Update courses.php

--- post-types/instructor/templates/single/parts/courses.php
+++ post-types/instructor/templates/single/parts/courses.php
@@ -1,5 +1,9 @@
-<?php if ( ! empty( $courses ) ) {
+<?php 
+/*
+if ( ! empty( $courses ) ) {
 	echo academist_elated_execute_shortcode( 'eltdf_course_list', $courses );
 } else {
 	esc_html_e( 'Sorry, no posts matched your criteria.', 'academist-lms' );
-}
+}*/
+
+do_action('single_instructor_courses');
